e6034e51f659f670a81e1209ef243a75f90eb4aa
Ritchie61 <124876391+Ritchie61@users.noreply.github.com>
2026-01-12 23:05:30 +1000

    Update App.js

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -5,16 +5,20 @@ import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "./screens/HomeScreen";
 import MapViewScreen from "./screens/MapViewScreen";
 import ReportHazardScreen from "./screens/ReportHazardScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import LeaderboardScreen from "./screens/LeaderboardScreen";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="MapView" component={MapViewScreen} />
         <Stack.Screen name="ReportHazard" component={ReportHazardScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="Leaderboard" component={LeaderboardScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
